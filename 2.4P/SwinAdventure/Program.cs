419c52b8b2845d7247e2d7fe8dacc047c553a30b
jjv7 <133873408+jjv7@users.noreply.github.com>
2024-07-04 13:21:00 +1000

    Added 2.4P files

---
+++ 2.4P/SwinAdventure/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SwinAdventure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IdentifiableObject id = new IdentifiableObject(new string[] { "id1", "id2" });    // Example of Identifiable Object
+        }
+    }
+}
