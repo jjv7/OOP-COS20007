2694ad1182165371e1fd3b1345789bbf497b7eeb
jjv7 <133873408+jjv7@users.noreply.github.com>
2024-07-04 13:01:37 +1000

    added 2.1P files

---
+++ 2.1P/SplashKitDemo/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using SplashKitSDK;
+
+namespace SplashKitDemo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            new Window("Test Window", 800, 600);
+            SplashKit.Delay(5000);
+
+        }
+    }
+}
